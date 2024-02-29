346a012ee4088a15212b38148586d796cbd35473
Heyu Fu <Herry Fu>
2024-03-01 10:16:09 +1100

    implement Create Customer and account functions

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,7 +1,7 @@
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function Customer() {
-  const fullname = useSelector((store) => store.customer.fullname);
+  const fullname = useSelector((store) => store.customer.fullName);
 
   return <h2>👋 Welcome, {fullname}</h2>;
 }
