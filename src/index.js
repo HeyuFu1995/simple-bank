f93188af01fd3a4a8d0a5a9225508714a9092c08
Heyu Fu <Herry Fu>
2024-03-01 09:51:22 +1100

    Import react-redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 import store from "./store/store";
 
@@ -9,6 +10,8 @@ import store from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
